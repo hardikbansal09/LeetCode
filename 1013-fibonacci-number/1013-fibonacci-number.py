19d64aaa9a49ed76551745b47965b1d972eb10ae
Hardik Bansal <102210776+hardikbansal09@users.noreply.github.com>
2024-07-17 15:24:53 +0530

    Time: 281 ms (35.85%), Space: 16.6 MB (11.27%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0:
+            return 0
+        elif n==1 or n==2:
+            return 1
+        else:
+            a=self.fib(n-2)+self.fib(n-1)
+        return a
+        
