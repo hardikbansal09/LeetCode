38e863b7b99a9e177d9a55c5507fd6f0c383cbf1
Hardik Bansal <102210776+hardikbansal09@users.noreply.github.com>
2024-08-02 18:34:57 +0530

    Time: 328 ms (65.07%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,9 @@
+select e.name    
+from Employee as e
+join (
+    select managerId
+    from Employee
+    where managerId is not null
+    group by managerId
+    having count(*)>=5
+)m on e.id=m.managerId;
