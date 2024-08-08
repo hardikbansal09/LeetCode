f2d4e6079291f83f21f66be13a34bb3c70bd1953
Hardik Bansal <102210776+hardikbansal09@users.noreply.github.com>
2024-08-08 09:56:16 +0530

    Time: 40 ms (87.31%), Space: 17.8 MB (48.07%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,6 +1,6 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        from collections import Counter
+        # from collections import Counter
         S=Counter(s)
         sorted_char=S.most_common()
         result=''.join([char*count for char,count in sorted_char])
